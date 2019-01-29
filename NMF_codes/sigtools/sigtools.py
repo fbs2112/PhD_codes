d73d2d55e9475405ed76ef7222b36f847258dc7b
fbs2112 <fbs@poli.ufrj.br>
2019-01-29 19:06:26 -0200

    Script with some signal and plot functions has been added

---
+++ NMF_codes/sigtools/sigtools.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.signal import butter, lfilter
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+    
+
+def db2pow(y):
+    return 10.**(y/10.)
+
+def pow_eval(y):
+    return (np.linalg.norm(y)**2)/len(y)
