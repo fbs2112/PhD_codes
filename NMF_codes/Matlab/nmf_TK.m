6e209ed5e4611e7e08db4ac4e04af085bbfb3f13
fbs2112 <fbs@poli.ufrj.br>
2019-04-02 14:55:20 +1100

    Small modifications, as in the previous commit

--- NMF_codes/Matlab/nmf_TK.m
+++ NMF_codes/Matlab/nmf_TK.m
@@ -2,14 +2,29 @@ clear;
 clc;
 close all;
 
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
+
 addpath(['.' filesep 'Sigtools' filesep])
+addpath(['.' filesep 'Sigtools' filesep 'NMF_algorithms'])
+
 
 figPath = './figs-02-14/';
 fs = 32.768e6;
-nfft = 19;
-nperseg = 19;
+nfft = 256;
+nperseg = 256;
 beta_loss = 'kullback-leibler';
 save_fig = 0;
+
+
+params.numberOfSources = 2;
+params.init = 'random';
+params.betaDivergence = 'frobenius';
+params.numberOfIterations = 10000;
+params.random_state = 42;
+params.tolChange = 1e-6;
+params.tolError = 1e-6;
+
 
 exp_name = 'TK';    
 f0 = 0;
@@ -18,32 +33,89 @@ numberOfSamples = secondsOfData*fs;
 totalSamples = 4096;
 bandwidth = 1e6;
 JNRVector = [-10, -5, 0, 10];
-JNRVector = 10;
+JNRVector = inf;
 
 
 t = 0:1/fs:(secondsOfData - 1/fs);
-f = ((bandwidth/2)/secondsOfData)*t + f0;
+% f = ((bandwidth/2)/secondsOfData)*t + f0;
+f = ((bandwidth/2)/secondsOfData) + f0;
+f1 = 1;
+f2 = 4;
 
-signal1 = exp(1j*2*pi*f.*t).';    
+signal1 = sin(2*pi*f1*f.*t).';    
 signal1 = repmat(signal1, ceil(100e-6/secondsOfData), 1);
-powSignal = pow_eval(signal1);
-signal1 = [zeros(totalSamples - length(signal1), 1); signal1];
-signalLength = length(signal1);
-noise = randn(signalLength, 1) + 1j*randn(signalLength, 1);
+signal2 = sin(2*pi*f2*f.*t).';  
+signal2 = repmat(signal2, ceil(100e-6/secondsOfData), 1);
+signal1Length = length(signal1);
+
+signal1 = [zeros(totalSamples - signal1Length, 1); signal1];
+signal2 = [signal2;zeros(totalSamples - signal1Length, 1)];
+mixtureSignal = signal1 + signal2;
+powMixture = pow_eval(mixtureSignal);
+
+noise = randn(totalSamples, 1) + 1j*randn(totalSamples, 1);
 powNoise = pow_eval(noise);
 
 for i = 1:length(JNRVector)
     
-    powAux = powSignal/JNRVector(i);
+    powAux = powMixture/JNRVector(i);
     noise2 = noise*sqrt(powAux/powNoise);
-    data = signal1 + noise2;
-    [PxxAux, f, t] = spectrogram(data, hann(nfft), nfft-1, nfft, fs, 'centered', 'power');
-    inputNMF = abs(PxxAux).^2;
-    specdB = 10*log10(inputNMF);
-%     spectrogram(data, hann(nfft), nfft-1, nfft, fs, 'centered', 'power');
-
-%     figure;
-%     imshow(PxxAux)
+    data = mixtureSignal + noise2;
+    
+    if all(isreal(data))
+        [PxxAux, f, t] = spectrogram(data, hann(nfft), nfft-1, nfft, fs, 'power');
+    else
+        [PxxAux, f, t] = spectrogram(data, hann(nfft), nfft-1, nfft, fs, 'centered', 'power');
+    end    
+    
+    dataCell{1} = PxxAux;
+    figure;
+    
+%     surf(t, f, 10*log10(abs(dataCell{1}).^2));
+    surf(t*1e6, f/1e6, 10*log10(abs(dataCell{1}).^2), 'EdgeColor', 'none');
+    axis xy;
+    axis tight;
+    colormap(jet); 
+    view(0,90);
+    ylabel('Frequency [MHz]');
+    xlabel('Time [\mu s]');
+    colorbar;
+    caxis([-120 20]);
+    
+%     dataCell{2} = TK_filtering(PxxAux);
+    
+    for j = 1:length(dataCell)
+        inputNMF = abs(dataCell{j}).^2;
+        specdB = 10*log10(inputNMF);
+        [W, H, reconstructError] = nmf_py3(inputNMF, params);
+        figure;
+        plot(f, W);
+        figure;
+        plot(t, H);
+        figure;
+        plot(reconstructError(reconstructError~=0));
+        
+        for k = 1:params.numberOfSources
+            separatedSignal = W(:,k)*H(k,:);
+            figure;
+            surf(t*1e6, f/1e6, 10*log10(abs(separatedSignal).^2), 'EdgeColor', 'none');
+            axis xy;
+            axis tight;
+            colormap(jet);
+            view(0,90);
+            ylabel('Frequency [MHz]');
+            xlabel('Time [\mu s]');
+            colorbar;
+        end
+        
+    end
+        
+    
+    
+%     spectrogram(data, hann(nfft), nfft-1, nfft, fs, 'centered', 'power', 'yaxis');
 end
 
+
+
+rmpath(['.' filesep 'Sigtools' filesep 'NMF_algorithms'])
 rmpath(['.' filesep 'Sigtools' filesep])
