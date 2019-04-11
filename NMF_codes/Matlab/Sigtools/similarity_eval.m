2d63210352c11efc4e3b60a2c632a0ad6da4e6ef
fbs2112 <fbs@poli.ufrj.br>
2019-04-11 14:11:07 +1000

    Similarity function has been added

---
+++ NMF_codes/Matlab/Sigtools/similarity_eval.m
@@ -0,0 +1,40 @@
+function [output] = similarity_eval(x, y, similarity, varargin)
+
+x = x(:);
+y = y(:);
+
+if nargin > 3
+    if varargin{2}
+         x = x - mean(x);
+         x =  x*sqrt(1/var(x));
+         y = y - mean(y);
+         y = y*sqrt(1/var(y));
+    end
+end
+
+switch similarity
+    case 'inner'
+        output = (x.'*y);
+        if strcmp(varargin{1}, 'normalized')
+            output = output/(norm(x)*norm(y));
+        end
+        
+    case 'gaussian'
+        if nargin < 3
+            error('Please choose a standard deviation value');
+        end
+        
+        sigma = varargin{1};
+        output =  exp(-(norm(x - y)^2)/(2*sigma^2));
+        
+    case 'polinomial'
+        if nargin < 3
+            error('Please choose the polinomial degree');
+        end
+        disp('Still needs to be implemented');
+        output = [];
+        
+    otherwise
+        error('Please type inner, gausssian or polinomial as similarity function');
+        
+end
--- NMF_codes/Matlab/Sigtools/window_eval.m
+++ NMF_codes/Matlab/Sigtools/window_eval.m
@@ -1,9 +1,8 @@
 
 function [window] = window_eval(y, window_length, function_eval, varargin)
 
+y = y(:);
 hop_size = 1;
-
-y(y==0) = eps;
 
 if nargin > 3
     hop_size = varargin{1};
@@ -11,16 +10,18 @@ end
 
 aux = (length(y) - window_length)/hop_size;
 
- if aux < 0
+if aux < 0
     error('Window size and hop size are incompatible.')
 end
 
-yAux = buffer(y, window_length, window_length - hop_size, 'nodelay');
+y = [y(1)*ones(window_length-1, 1); y]; % padding with the first element of y
+y(y==0) = eps;
+
+yAux = flipud(buffer(y, window_length, window_length - hop_size, 'nodelay'));
 
 lastColumn = yAux(:, end);
 
 idxNonzero = find(lastColumn, 1, 'last');
 idxZero = find(~lastColumn);
 yAux(idxZero,end) = lastColumn(idxNonzero);
-% number_of_windows = ceil(aux + 1);
 window = function_eval(yAux, 1);
