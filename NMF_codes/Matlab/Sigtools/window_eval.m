3db9520343a268e367de2fd47dc6d08219c47c98
fbs2112 <fbs@pol.ufrj.br>
2019-05-27 17:43:42 +1000

    Small fixes

--- NMF_codes/Matlab/Sigtools/window_eval.m
+++ NMF_codes/Matlab/Sigtools/window_eval.m
@@ -14,15 +14,17 @@ if aux < 0
     error('Window size and hop size are incompatible.')
 end
 
-y = [y(1)*ones(window_length-1, 1); y]; % padding with the first element of y
-y(y==0) = eps;
+inputLength = length(y);
+y = [y(1)*ones(window_length-1, 1); y ; y(end)*ones(window_length-1, 1)]; % padding with the first element of y
+% y(y==0) = eps;
 
 yAux = buffer(y, window_length, window_length - hop_size, 'nodelay');
+window = function_eval(yAux(:, 1:inputLength));
 
-lastColumn = yAux(:, end);
-
-idxNonzero = find(lastColumn, 1, 'last');
-idxZero = find(~lastColumn);
-yAux(idxZero,end) = lastColumn(idxNonzero);
-window = function_eval(yAux, 1);
-window(window<=eps) = 0;
+% lastColumn = yAux(:, end);
+% 
+% idxNonzero = find(lastColumn, 1, 'last');
+% idxZero = find(~lastColumn);
+% yAux(idxZero,end) = lastColumn(idxNonzero);
+% window = function_eval(yAux, 1);
+% window(window<=eps) = 0;
