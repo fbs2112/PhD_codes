74ea25021e9ad3da99a019561dd168c4e247bf93
fbs2112 <fbs@poli.ufrj.br>
2019-06-07 01:15:12 +1000

    Modifications to deal with border effects

--- NMF_codes/Matlab/Sigtools/TK_eval.m
+++ NMF_codes/Matlab/Sigtools/TK_eval.m
@@ -8,6 +8,8 @@ for i = 1:size(x,2)
     aux = [y(1); y(1:end-1)];
     aux2 = [y(2:end); y(end)];
     a(:,i) = b - aux2.*aux;
+    a(1,i) = a(2,i);  %smoothing border effects
+    a(end,i) = a(end-1,i); %smoothing border effects
 end
 
 output = a;
