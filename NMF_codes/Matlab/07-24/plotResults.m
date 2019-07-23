7935d357e323aac9f18f51d290e71ea8ecc22334
fbs2112 <fbs@pol.ufrj.br>
2019-07-23 17:57:00 +1000

    Mild update in the plot code

--- NMF_codes/Matlab/07-24/plotResults.m
+++ NMF_codes/Matlab/07-24/plotResults.m
@@ -69,6 +69,14 @@ for i = 1:1%length(JNRVector)
     disp('------------------------------------')
 end
 clc;
+%%
+
+clear;
+clc;
+close all;
+
+addpath(['..' filesep '.' filesep 'Misc'])
+addpath(['..' filesep '.' filesep 'data' filesep '07-24']);    
 
 load results02.mat;
 
@@ -135,6 +143,7 @@ for JNRIndex = 1:length(JNRVector)
             acc = (tp + tn)./(tp + fn + tn + fp);
             f1Score = 2*tp./(2*tp + fp + fn);
             precision = tp./(tp+fp);
+            precision(isnan(precision)) = 0;
             tpr = tp./(tp+fn);
             fpr = fp./(fp+tn);
             
@@ -264,8 +273,16 @@ xlabel('Probability of false alarm');
 ylabel('Probability of detection');
 legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
 
+rmpath(['..' filesep '.' filesep 'Misc'])
+rmpath(['..' filesep '.' filesep 'data' filesep '07-24']);    
 
+%%
+clear;
 clc;
+close all;
+
+addpath(['..' filesep '.' filesep 'Misc'])
+addpath(['..' filesep '.' filesep 'data' filesep '07-24']); 
 
 load results03.mat;
 
@@ -332,6 +349,7 @@ for JNRIndex = 1:length(JNRVector)
             acc = (tp + tn)./(tp + fn + tn + fp);
             f1Score = 2*tp./(2*tp + fp + fn);
             precision = tp./(tp+fp);
+            precision(isnan(precision)) = 0;
             tpr = tp./(tp+fn);
             fpr = fp./(fp+tn);
             
@@ -659,7 +677,19 @@ xlabel('Probability of false alarm');
 ylabel('Probability of detection');
 legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
 
+rmpath(['..' filesep '.' filesep 'Misc'])
+rmpath(['..' filesep '.' filesep 'data' filesep '07-24']);   
 
+%%
+%Count true positives, negatives, false positives, negatives sample by
+%sample
+
+clear;
+clc;
+close all;
+
+addpath(['..' filesep '.' filesep 'Misc'])
+addpath(['..' filesep '.' filesep 'data' filesep '07-24']);    
 
 load results03.mat;
 
@@ -859,7 +889,6 @@ close all;
 addpath(['..' filesep '.' filesep 'Misc'])
 addpath(['..' filesep '.' filesep 'data' filesep '07-24']);  
 
-load_aux = matfile('results04.mat');
 
 onset = 528;
 offset = 3911;
@@ -890,6 +919,8 @@ stdPrec = zeros(length(JNRVector), length(thresholdVector), length(stdVector), l
 
 for JNRIndex = 1:length(JNRVector)
     for thresholdIndex = 1:length(thresholdVector)
+        thresholdIndex
+        load(['results04_' num2str(thresholdIndex) '.mat'])
         for stdIndex = 1:length(stdVector)
             for window_median_length_index = 1:length(window_median_length_vector)
                 
@@ -897,12 +928,12 @@ for JNRIndex = 1:length(JNRVector)
                 tp = zeros(monteCarloLoops, 1);
                 fn = zeros(monteCarloLoops, 1);
                 tn = zeros(monteCarloLoops, 1);
-                aux = squeeze(load_aux.detection_res(:, JNRIndex, thresholdIndex, stdIndex, window_median_length_index, :));
+                detection_res = squeeze(aux(:, JNRIndex, 1, stdIndex, window_median_length_index, :));
                 for loopIndex = 1:monteCarloLoops
                     
-                    fpRegion1 =  aux(loopIndex, 1:onset);
-                    tpRegion = aux(loopIndex, onset  + round(window_length*2):offset - round(window_length*2));
-                    fpRegion2 = aux(loopIndex, offset + round(window_length*2):end);
+                    fpRegion1 =  detection_res(loopIndex, 1:onset);
+                    tpRegion = detection_res(loopIndex, onset  + round(window_length*2):offset - round(window_length*2));
+                    fpRegion2 = detection_res(loopIndex, offset + round(window_length*2):end);
                     
                     if any(fpRegion1)
                         fp(loopIndex) = fp(loopIndex) + 1;
@@ -927,6 +958,7 @@ for JNRIndex = 1:length(JNRVector)
                 acc = (tp + tn)./(tp + fn + tn + fp);
                 f1Score = 2*tp./(2*tp + fp + fn);
                 precision = tp./(tp+fp);
+                precision(isnan(precision)) = 0;
                 tpr = tp./(tp+fn);
                 fpr = fp./(fp+tn);
                 
@@ -973,7 +1005,7 @@ for i = 1:length(JNRVector)
     disp('------------------------------------')
     disp(['Threshold: ' num2str(thresholdVector(j))])
     disp('------------------------------------')
-    disp(['Std: ' num2str(thresholdVector(k))])
+    disp(['Std: ' num2str(stdVector(k))])
     disp('------------------------------------')
     disp(['Window length: ' num2str(window_median_length_vector(l))])
     disp('------------------------------------')
@@ -991,28 +1023,29 @@ xlabel('Probability of false alarm');
 ylabel('Probability of detection');
 legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
 
-
 figure;
 for i = 1:length(JNRVector)
-    averageF1Aux = squeeze(averageF1(i,:,:));
+    averageF1Aux = squeeze(averageF1(i,:,:,:));
 
     [maxval, maxidx] = max(averageF1Aux(:));
-    [j, k] = ind2sub(size(averageF1Aux), maxidx);
+    [j, k, l] = ind2sub(size(averageF1Aux), maxidx);
     disp('---------------------F1-score-----------------------------------')
     disp('------------------------------------')
     disp(['-------------JNR: ' num2str(JNRVector(i)) ' dB-----------------------'])
-    disp(['F1-score: ' num2str(maxval) '+-' num2str(stdF1(i,j,k))])
+    disp(['F1-score: ' num2str(maxval) '+-' num2str(stdF1(i,j,k,l))])
     disp('------------------------------------')
-    disp(['Pd: ' num2str(averageTPR(i,j,k)) '+-' num2str(stdTPR(i,j,k))])
+    disp(['Pd: ' num2str(averageTPR(i,j,k,l)) '+-' num2str(stdTPR(i,j,k,l))])
     disp('------------------------------------')
-    disp(['Pfa: ' num2str(averageFPR(i,j,k)) '+-' num2str(stdFPR(i,j,k))])
+    disp(['Pfa: ' num2str(averageFPR(i,j,k,l)) '+-' num2str(stdFPR(i,j,k,l))])
     disp('------------------------------------')
     disp(['Threshold: ' num2str(thresholdVector(j))])
     disp('------------------------------------')
-    disp(['Window length: ' num2str(window_median_length_vector(k))])
+    disp(['Std: ' num2str(stdVector(k))])
+    disp('------------------------------------')
+    disp(['Window length: ' num2str(window_median_length_vector(l))])
     disp('------------------------------------')
     
-    plot(averageFPR(i,:,k), averageTPR(i,:,k))
+    plot(averageFPR(i,:,k,l), averageTPR(i,:,k,l))
     hold on    
 end
 
@@ -1028,26 +1061,236 @@ legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location'
 
 figure;
 for i = 1:length(JNRVector)
-    averagePrecAux = squeeze(averagePrec(i,:,:));
+    averagePrecAux = squeeze(averagePrec(i,:,:,:));
 
-    [maxval, maxidx] = max(averagePrecAux(:), [], 'includenan');
-    [j, k] = ind2sub(size(averagePrecAux), maxidx);
+    [maxval, maxidx] = max(averagePrecAux(:));
+    [j, k, l] = ind2sub(size(averagePrecAux), maxidx);
     disp('---------------------Precision-----------------------------------')
     disp('------------------------------------')
     disp(['-------------JNR: ' num2str(JNRVector(i)) ' dB-----------------------'])
-    disp(['Precision: ' num2str(maxval) '+-' num2str(stdPrec(i,j,k))])
+    disp(['Precision: ' num2str(maxval) '+-' num2str(stdPrec(i,j,k,l))])
     disp('------------------------------------')
-    disp(['Pd: ' num2str(averageTPR(i,j,k)) '+-' num2str(stdTPR(i,j,k))])
+    disp(['Pd: ' num2str(averageTPR(i,j,k,l)) '+-' num2str(stdTPR(i,j,k,l))])
     disp('------------------------------------')
-    disp(['Pfa: ' num2str(averageFPR(i,j,k)) '+-' num2str(stdFPR(i,j,k))])
+    disp(['Pfa: ' num2str(averageFPR(i,j,k,l)) '+-' num2str(stdFPR(i,j,k,l))])
     disp('------------------------------------')
     disp(['Threshold: ' num2str(thresholdVector(j))])
     disp('------------------------------------')
-    disp(['Window length: ' num2str(window_median_length_vector(k))])
+    disp(['Std: ' num2str(stdVector(k))])
+    disp('------------------------------------')
+    disp(['Window length: ' num2str(window_median_length_vector(l))])
     disp('------------------------------------')
     
-    plot(averageFPR(i,:,k), averageTPR(i,:,k))
-    hold on 
+    plot(averageFPR(i,:,k,l), averageTPR(i,:,k,l))
+    hold on    
+end
+
+xAux = linspace(0, 1, numel(averageTPR(i, :, k)));
+yAux = linspace(0, 1, numel(averageFPR(i, :, k)));
+plot(xAux, yAux, '--');
+box 'off'
+grid on
+xlabel('Probability of false alarm');
+ylabel('Probability of detection');
+legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
+
+rmpath(['..' filesep '.' filesep 'data' filesep '07-24']);    
+rmpath(['..' filesep '.' filesep 'Misc'])
+%%
+%Count true positives, negatives, false positives, negatives sample by
+%sample
+
+clear;
+clc;
+close all;
+
+addpath(['..' filesep '.' filesep 'Misc'])
+addpath(['..' filesep '.' filesep 'data' filesep '07-24']);  
+
+
+onset = 528;
+offset = 3911;
+window_length = 98;
+
+thresholdVector = 0.1:0.05:0.9;
+window_median_length_vector = 51:50:401;
+stdVector = 7:13;
+monteCarloLoops = 100;
+JNRVector = [-20 -15 -10 -5 0];
+
+averageFP = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averageTN = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averageTP = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averageFN = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+
+averageTPR = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averageFPR = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+stdTPR = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+stdFPR = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+
+averageACC = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+stdACC = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averageF1 = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+stdF1 = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+averagePrec = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+stdPrec = zeros(length(JNRVector), length(thresholdVector), length(stdVector), length(window_median_length_vector));
+
+for JNRIndex = 1:length(JNRVector)
+    for thresholdIndex = 1:length(thresholdVector)
+        thresholdIndex
+        load(['results04_' num2str(thresholdIndex) '.mat'])
+        for stdIndex = 1:length(stdVector)
+            for window_median_length_index = 1:length(window_median_length_vector)
+                
+                fp = zeros(monteCarloLoops, 1);
+                tp = zeros(monteCarloLoops, 1);
+                fn = zeros(monteCarloLoops, 1);
+                tn = zeros(monteCarloLoops, 1);
+                detection_res = squeeze(aux(:, JNRIndex, 1, stdIndex, window_median_length_index, :));
+                for loopIndex = 1:monteCarloLoops
+                    
+                    fpRegion1 =  detection_res(loopIndex, 1:onset);
+                    tpRegion = detection_res(loopIndex, onset  + round(window_length*2):offset - round(window_length*2));
+                    fpRegion2 = detection_res(loopIndex, offset + round(window_length*2):end);
+                    
+                    fpRegion = [fpRegion1 fpRegion2];
+                    
+                    fp(loopIndex) = sum(fpRegion);
+                    tn(loopIndex) = length(fpRegion) - fp(loopIndex);
+                    
+                    tp(loopIndex) = sum(tpRegion);
+                    fn(loopIndex) = length(tpRegion) - tp(loopIndex);
+                    
+                end
+                
+                acc = (tp + tn)./(tp + fn + tn + fp);
+                f1Score = 2*tp./(2*tp + fp + fn);
+                precision = tp./(tp+fp);
+                precision(isnan(precision)) = 0;
+                tpr = tp./(tp+fn);
+                fpr = fp./(fp+tn);
+                
+                fpStd = std(fp);
+                tnStd = std(tn);
+                tpStd = std(tp);
+                fnStd = std(fn);
+                
+                averageFP(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(fp);
+                averageTN(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(tn);
+                averageTP(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(tp);
+                averageFN(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(fn);
+                
+                averageTPR(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(tpr);
+                averageFPR(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(fpr);
+                stdTPR(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = std(tpr);
+                stdFPR(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = std(fpr);
+                
+                averageACC(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(acc);
+                stdACC(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = std(acc);
+                averageF1(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(f1Score);
+                stdF1(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = std(f1Score);
+                averagePrec(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = mean(precision);
+                stdPrec(JNRIndex, thresholdIndex, stdIndex, window_median_length_index) = std(precision);
+            end
+        end
+    end
+end
+
+figure;
+for i = 1:length(JNRVector)
+    averageACCAux = squeeze(averageACC(i,:,:,:));
+
+    [maxval, maxidx] = max(averageACCAux(:));
+    [j, k, l] = ind2sub(size(averageACCAux), maxidx);
+    disp('---------------------Accuracy-----------------------------------')
+    disp('------------------------------------')
+    disp(['-------------JNR: ' num2str(JNRVector(i)) ' dB-----------------------'])
+    disp(['Accuracy: ' num2str(maxval) '+-' num2str(stdACC(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pd: ' num2str(averageTPR(i,j,k,l)) '+-' num2str(stdTPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pfa: ' num2str(averageFPR(i,j,k,l)) '+-' num2str(stdFPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Threshold: ' num2str(thresholdVector(j))])
+    disp('------------------------------------')
+    disp(['Std: ' num2str(stdVector(k))])
+    disp('------------------------------------')
+    disp(['Window length: ' num2str(window_median_length_vector(l))])
+    disp('------------------------------------')
+    
+    plot(averageFPR(i,:,k,l), averageTPR(i,:,k,l))
+    hold on    
+end
+
+xAux = linspace(0, 1, numel(averageTPR(i, :, k, l)));
+yAux = linspace(0, 1, numel(averageFPR(i, :, k, l)));
+plot(xAux, yAux, '--');
+box 'off'
+grid on
+xlabel('Probability of false alarm');
+ylabel('Probability of detection');
+legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
+
+figure;
+for i = 1:length(JNRVector)
+    averageF1Aux = squeeze(averageF1(i,:,:,:));
+
+    [maxval, maxidx] = max(averageF1Aux(:));
+    [j, k, l] = ind2sub(size(averageF1Aux), maxidx);
+    disp('---------------------F1-score-----------------------------------')
+    disp('------------------------------------')
+    disp(['-------------JNR: ' num2str(JNRVector(i)) ' dB-----------------------'])
+    disp(['F1-score: ' num2str(maxval) '+-' num2str(stdF1(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pd: ' num2str(averageTPR(i,j,k,l)) '+-' num2str(stdTPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pfa: ' num2str(averageFPR(i,j,k,l)) '+-' num2str(stdFPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Threshold: ' num2str(thresholdVector(j))])
+    disp('------------------------------------')
+    disp(['Std: ' num2str(stdVector(k))])
+    disp('------------------------------------')
+    disp(['Window length: ' num2str(window_median_length_vector(l))])
+    disp('------------------------------------')
+    
+    plot(averageFPR(i,:,k,l), averageTPR(i,:,k,l))
+    hold on    
+end
+
+xAux = linspace(0, 1, numel(averageTPR(i, :, k)));
+yAux = linspace(0, 1, numel(averageFPR(i, :, k)));
+plot(xAux, yAux, '--');
+box 'off'
+grid on
+xlabel('Probability of false alarm');
+ylabel('Probability of detection');
+legend('-20 dB', '-15 dB', '-10 dB', '-5 dB', '0 dB', 'Random guess', 'Location', 'southeast');
+
+
+figure;
+for i = 1:length(JNRVector)
+    averagePrecAux = squeeze(averagePrec(i,:,:,:));
+
+    [maxval, maxidx] = max(averagePrecAux(:));
+    [j, k, l] = ind2sub(size(averagePrecAux), maxidx);
+    disp('---------------------Precision-----------------------------------')
+    disp('------------------------------------')
+    disp(['-------------JNR: ' num2str(JNRVector(i)) ' dB-----------------------'])
+    disp(['Precision: ' num2str(maxval) '+-' num2str(stdPrec(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pd: ' num2str(averageTPR(i,j,k,l)) '+-' num2str(stdTPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Pfa: ' num2str(averageFPR(i,j,k,l)) '+-' num2str(stdFPR(i,j,k,l))])
+    disp('------------------------------------')
+    disp(['Threshold: ' num2str(thresholdVector(j))])
+    disp('------------------------------------')
+    disp(['Std: ' num2str(stdVector(k))])
+    disp('------------------------------------')
+    disp(['Window length: ' num2str(window_median_length_vector(l))])
+    disp('------------------------------------')
+    
+    plot(averageFPR(i,:,k,l), averageTPR(i,:,k,l))
+    hold on    
 end
 
 xAux = linspace(0, 1, numel(averageTPR(i, :, k)));
