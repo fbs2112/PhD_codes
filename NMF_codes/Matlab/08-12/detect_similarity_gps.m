a591f2b5bf6c9756bd4a514b18454acff50ce3cc
fbs2112 <fbs@pol.ufrj.br>
2019-08-05 15:21:42 +1000

    Script has been updated to compare results with Pai's setup

--- NMF_codes/Matlab/08-12/detect_similarity_gps.m
+++ NMF_codes/Matlab/08-12/detect_similarity_gps.m
@@ -9,17 +9,14 @@ addpath(['..' filesep 'signalsGeneration' filesep 'sim_params']);
 
 load sim_params_1.mat;
 
-fs = paramsSignal.Freqsamp;
-
-numberOfSources = 1;
 random_state = 42;
 
-params.fs = fs;
+params.fs = paramsSignal.Freqsamp;
 params.nfft = 128;
 params.nperseg = 128;
 params.overlap = params.nperseg-1;
 params.hop_size = params.nperseg - params.overlap;
-params.numberOfSources = numberOfSources;
+params.numberOfSources = 1;
 params.init = 'random';
 params.betaDivergence = 'kullback-leibler';
 params.numberOfIterations = 10000;
@@ -29,24 +26,14 @@ params.repetitions = 1;
 SNR = -25;
 params.JNRVector = [-20 -15 -10 -5 0];
 
+bandwidthVector = 10.72e6;
+periodVector = 8.72e-6;
+
 rng(random_state)
 
+initialFrequency = 2e6;
 numberOfRawSamples = 4096;
-silenceSamples = round(20e-6*fs);
-
-[interferenceSignal, GPSSignals] = signalGen(paramsSignal);
-GPSSignals = GPSSignals(1:numberOfRawSamples,:);
-interferenceSignal = interferenceSignal(1:numberOfRawSamples);
-
-GPSSignals = [zeros(silenceSamples, size(GPSSignals, 2)); GPSSignals; zeros(silenceSamples, size(GPSSignals, 2))];
-interferenceSignal = [zeros(silenceSamples, 1); interferenceSignal; zeros(silenceSamples, 1)];
-
-onset = find(GPSSignals(:,1), 1, 'first') - params.nperseg;
-offset = find(GPSSignals(:,1), 1, 'last') - params.nperseg;
-
-interferenceSignalPower = pow_eval(interferenceSignal);
-GPSSignalsPower = pow_eval(GPSSignals);
-
+silenceSamples = round(20e-6*params.fs);
 thresholdVector = 0.1:0.05:0.9;
 window_median_length_vector = 51:50:401;
 monteCarloLoops = 100;
@@ -60,41 +47,64 @@ for loopIndex = 1:monteCarloLoops
     noise = randn(numberOfRawSamples + silenceSamples*2, 1) + 1j*randn(numberOfRawSamples + silenceSamples*2, 1);
     noisePower = pow_eval(noise);
     
-    for i = 1:length(params.JNRVector)
-        GPSSignalsAux = GPSSignals;
-        interferenceSignalAux = interferenceSignal;
-        GPSMultiplier = sqrt(noisePower*10.^(SNR/10)./GPSSignalsPower);
-        mixtureGPS = sum(GPSSignalsAux.*GPSMultiplier, 2) + noise;
-        interferenceSignalAux = interferenceSignalAux*sqrt(noisePower*10^(params.JNRVector(i)/10)/interferenceSignalPower);
-        mixtureSignal(:,i) = mixtureGPS + interferenceSignalAux;
-    end
-    
-    [W, ~, ~, PxxAux, ~, ~] = nmf_eval_v2(mixtureSignal, params);
-    
-    for JNRIndex = 1:length(params.JNRVector)
-        
-        inputNMF = abs(PxxAux{1, JNRIndex}).^2;
-        
-        inputNMF = inputNMF - mean(inputNMF);
-        inputNMF = inputNMF.*sqrt(1./var(inputNMF));
-        inputNMFAux = sqrt(sum(inputNMF.*inputNMF)) + eps;
-        inputNMFNormalised = inputNMF./inputNMFAux;
-        
-        WNormalised = W{1, JNRIndex}(:,1) - mean(W{1, JNRIndex}(:,1));
-        WNormalised = WNormalised.*sqrt(1./var(WNormalised));
-        WNormalised = WNormalised ./ (norm(WNormalised) + eps);
-        
-        output = inputNMFNormalised.'*WNormalised;
-        for thresholdIndex = 1:length(thresholdVector)
-            for window_median_length_index = 1:length(window_median_length_vector)
-                detection_res(loopIndex, JNRIndex, thresholdIndex, window_median_length_index, :) = detection_eval(output, thresholdVector(thresholdIndex), ...
-                    window_median_length_vector(window_median_length_index));
+    for bandwidthIndex = 1:length(bandwidthVector)
+        for periodIndex = 1:length(periodVector)
+            paramsSignal.Noneperiod = round(periodVector(periodIndex)*params.fs);                   % number of samples with a sweep time
+            paramsSignal.IFmin = initialFrequency;                                                  % start frequency
+            paramsSignal.IFmax = bandwidthVector(bandwidthIndex) - periodVector;                    % end frequency
+            paramsSignal.foneperiod(1:paramsSignal.Noneperiod) = linspace(paramsSignal.IFmin, paramsSignal.IFmax, paramsSignal.Noneperiod);
+            paramsSignal.Initphase = 0;
+            
+            [interferenceSignal, GPSSignals] = signalGen(paramsSignal);
+            GPSSignals = GPSSignals(1:numberOfRawSamples,:);
+            interferenceSignal = interferenceSignal(1:numberOfRawSamples);
+            
+            GPSSignals = [zeros(silenceSamples, size(GPSSignals, 2)); GPSSignals; zeros(silenceSamples, size(GPSSignals, 2))];
+            interferenceSignal = [zeros(silenceSamples, 1); interferenceSignal; zeros(silenceSamples, 1)];
+            
+            onset = find(GPSSignals(:,1), 1, 'first') - params.nperseg;
+            offset = find(GPSSignals(:,1), 1, 'last') - params.nperseg;
+            
+            interferenceSignalPower = pow_eval(interferenceSignal);
+            GPSSignalsPower = pow_eval(GPSSignals);
+            
+            for i = 1:length(params.JNRVector)
+                GPSSignalsAux = GPSSignals;
+                interferenceSignalAux = interferenceSignal;
+                GPSMultiplier = sqrt(noisePower*10.^(SNR/10)./GPSSignalsPower);
+                mixtureGPS = sum(GPSSignalsAux.*GPSMultiplier, 2) + noise;
+                interferenceSignalAux = interferenceSignalAux*sqrt(noisePower*10^(params.JNRVector(i)/10)/interferenceSignalPower);
+                mixtureSignal(:,i) = mixtureGPS + interferenceSignalAux;
+            end
+            
+            [W, ~, ~, PxxAux, ~, ~] = nmf_eval_v2(mixtureSignal, params);
+            
+            for JNRIndex = 1:length(params.JNRVector)
+                
+                inputNMF = abs(PxxAux{1, JNRIndex}).^2;
+                
+                inputNMF = inputNMF - mean(inputNMF);
+                inputNMF = inputNMF.*sqrt(1./var(inputNMF));
+                inputNMFAux = sqrt(sum(inputNMF.*inputNMF)) + eps;
+                inputNMFNormalised = inputNMF./inputNMFAux;
+                
+                WNormalised = W{1, JNRIndex}(:,1) - mean(W{1, JNRIndex}(:,1));
+                WNormalised = WNormalised.*sqrt(1./var(WNormalised));
+                WNormalised = WNormalised ./ (norm(WNormalised) + eps);
+                
+                output = inputNMFNormalised.'*WNormalised;
+                for thresholdIndex = 1:length(thresholdVector)
+                    for window_median_length_index = 1:length(window_median_length_vector)
+                        detection_res(loopIndex, bandwidthIndex, periodIndex, JNRIndex, thresholdIndex, window_median_length_index, :) = ...
+                            detection_eval(output, thresholdVector(thresholdIndex), window_median_length_vector(window_median_length_index));
+                    end
+                end
             end
         end
     end
 end
 
-save(['..' filesep '.' filesep 'data' filesep '08-12' filesep 'results02.mat'], 'detection_res', '-v7.3');
+save(['..' filesep '.' filesep 'data' filesep '08-12' filesep 'results03.mat'], 'detection_res', '-v7.3');
 
 rmpath(['..' filesep 'signalsGeneration' filesep 'sim_params']);
 rmpath(['..' filesep 'signalsGeneration' filesep]);
