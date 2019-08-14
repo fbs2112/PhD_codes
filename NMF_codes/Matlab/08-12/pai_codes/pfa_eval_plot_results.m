23ca46f3b8d9e277867ef7103726741ad980be9b
fbs2112 <fbs@poli.ufrj.br>
2019-08-15 01:29:49 +1000

    Codes to evaluate probability of false alarm for Pai's technique has been added

---
+++ NMF_codes/Matlab/08-12/pai_codes/pfa_eval_plot_results.m
@@ -0,0 +1,51 @@
+clear;
+clc;
+close all;
+
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaulttextInterpreter','latex')
+
+
+addpath(['.' filesep 'data']);
+
+load resultsPai07.mat;
+
+PfaVector = logspace(-5, 0, 17);
+
+for k = 1:2    
+    for i = 1:length(PfaVector)
+        x = squeeze(GoFBlockDeteflagCell{k, 1}(1,1,i,:,:));
+        for j = 1:size(x, 2)
+            fp(k,j,i) = sum(x(:,j));
+            tn(k,j,i) = size(x(:,j), 1) - fp(k,j,i);
+        end
+    end
+
+end
+fpr = fp./(fp+tn);
+
+averageFpr = squeeze(mean(fpr, 2));
+stdFpr = squeeze(std(fpr, [], 2));
+
+figure;
+plot(PfaVector, averageFpr(1,:));
+hold on;
+plot(PfaVector, averageFpr(2,:));
+
+xAux = linspace(0, 1, size(averageFpr, 2));
+yAux = linspace(0, 1, size(averageFpr, 2));
+plot(xAux, yAux, '--');
+legend('$L = 3$', '$L = 19$', 'Random guess'); 
+
+figure;
+loglog(PfaVector, averageFpr(1,:));
+hold on;
+loglog(PfaVector, averageFpr(2,:));
+
+xAux = logspace(-5, 0, size(averageFpr, 2));
+yAux = logspace(-5, 0, size(averageFpr, 2));
+plot(xAux, yAux, '--');
+legend('$L = 3$', '$L = 19$', 'Random guess'); 
+
+rmpath(['.' filesep 'data']);
