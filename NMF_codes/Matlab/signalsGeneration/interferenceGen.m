970b2050083772d6673de02a686e9f1406b2a36b
fbs2112 <fbs@pol.ufrj.br>
2019-08-16 14:11:21 +1000

    Signal gen function has been split into two new function. GPS and interference signals are now generated separately

---
+++ NMF_codes/Matlab/signalsGeneration/interferenceGen.m
@@ -0,0 +1,8 @@
+function interferenceSignal = interferenceGen(params)
+
+Endtimelastloop = -1;
+Timeofthisloop = Endtimelastloop + (1:params.Intenumb);        
+% Interference signal
+Phasemod = mod(Timeofthisloop, params.Noneperiod)+1;
+iflaw = (params.foneperiod(Phasemod)/params.Freqsamp)';
+[interferenceSignal, ~] = fmodany(iflaw);
