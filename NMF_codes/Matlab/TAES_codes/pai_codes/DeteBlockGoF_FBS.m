14db1e71e5403f2ebf9e777cc0ba032a1210fdf4
fbs2112 <fbs@poli.ufrj.br>
2019-08-19 02:08:14 +1000

    Pai code for block stft detection has been rewritten

---
+++ NMF_codes/Matlab/TAES_codes/pai_codes/DeteBlockGoF_FBS.m
@@ -0,0 +1,14 @@
+function GoFBlockDeteflag = DeteBlockGoF_FBS(signal, h, MBlock, PfaVector)
+
+GoFBlockDeteflag = zeros(MBlock, length(PfaVector));
+[tfr,~,~] = tfrstftblock(signal, 1:MBlock, MBlock, h);
+
+for k = 1:MBlock
+    [~, GoFBlockpvalue] = chi2gof((abs(tfr(k,:))).^2,'cdf',{@chi2cdf,2},'nparams',0);
+    for pfaIndex = 1:length(PfaVector)
+        if GoFBlockpvalue < PfaVector(pfaIndex)
+            GoFBlockDeteflag(k, pfaIndex) = 1;
+        end
+    end
+end
+
