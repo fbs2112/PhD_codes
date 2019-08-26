17c4e5d3f85d94cb63dfb7ba926e2fe0ada008d9
fbs2112 <fbs@pol.ufrj.br>
2019-08-26 17:55:56 +1000

    Modifications to save p-value have been performed

--- NMF_codes/Matlab/TAES_codes/pai_codes/DeteBlockGoF_FBS.m
+++ NMF_codes/Matlab/TAES_codes/pai_codes/DeteBlockGoF_FBS.m
@@ -1,14 +1,15 @@
-function GoFBlockDeteflag = DeteBlockGoF_FBS(signal, h, MBlock, PfaVector)
+function [pvalue, GoFBlockDeteflag] = DeteBlockGoF_FBS(signal, h, MBlock, PfaVector)
 
 GoFBlockDeteflag = zeros(MBlock, length(PfaVector));
+pvalue = zeros(MBlock, 1);
 [tfr,~,~] = tfrstftblock(signal, 1:MBlock, MBlock, h);
 
 for k = 1:MBlock
     [~, GoFBlockpvalue] = chi2gof((abs(tfr(k,:))).^2,'cdf',{@chi2cdf,2},'nparams',0);
+    pvalue(k) = GoFBlockpvalue;
     for pfaIndex = 1:length(PfaVector)
         if GoFBlockpvalue < PfaVector(pfaIndex)
             GoFBlockDeteflag(k, pfaIndex) = 1;
         end
     end
 end
-
