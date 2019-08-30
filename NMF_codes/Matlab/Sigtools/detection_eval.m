a1c8b2955211cef89e9754cb57457e0d1b71f7d1
fbs2112 <fbs@pol.ufrj.br>
2019-08-30 17:59:03 +1000

    Detection eval function has been updated to cope with inverted detection logic

--- NMF_codes/Matlab/Sigtools/detection_eval.m
+++ NMF_codes/Matlab/Sigtools/detection_eval.m
@@ -3,7 +3,14 @@ function [output] = detection_eval(y, threshold, varargin)
 output = false(length(y), 1);
 output(y > threshold) = true;
 
-if nargin > 2
+if nargin > 3
+    if strcmp(varargin{2}, 'invert')
+        output = false(length(y), 1);
+        output(y < threshold) = true;
+    end
+end
+
+if nargin == 3
     if ~mod(varargin{1}, 2)
         error('Window length must be odd');
     end
