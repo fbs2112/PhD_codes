dda955754fdb0930d2241a78cccce674729d32d8
fbs2112 <fbs@pol.ufrj.br>
2019-09-02 17:17:04 +1000

    Scripts to evaluate time per iteration

---
+++ NMF_codes/Matlab/TAES_codes/my_codes/plotResultsTime.m
@@ -0,0 +1,62 @@
+clear;
+clc;
+close all;
+
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaulttextInterpreter','latex')
+
+addpath(['..' filesep '..' filesep '.' filesep 'Misc'])
+if isunix
+    addpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+        filesep 'time_results' filesep]);
+    
+else
+    addpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+        filesep 'time_results' filesep])
+end  
+
+load resultsTime1.mat;
+disp(['NMF detector average time: ' num2str(mean(elapsedTime)*1e3) ' +- ' num2str(var(elapsedTime)*1e3) ' ms']);
+
+if isunix
+    rmpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+        filesep 'time_results' filesep]);
+    
+else
+    rmpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+        filesep 'time_results' filesep])
+end  
+
+
+if isunix
+    addpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'pai_results' ...
+        filesep 'time_results' filesep]);
+    
+else
+    addpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'pai_results' ...
+        filesep 'time_results' filesep])
+
+end  
+
+load resultsTime1.mat;
+disp(['Pai''s average time (L = 3): ' num2str(mean(elapsedTime)*1e3) ' +- ' num2str(var(elapsedTime)*1e3) ' ms']);
+load resultsTime2.mat;
+disp(['Pai''s average time (L = 19): ' num2str(mean(elapsedTime)*1e3) ' +- ' num2str(var(elapsedTime)*1e3) ' ms']);
+
+if isunix
+    rmpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'pai_results' ...
+        filesep 'time_results' filesep]);
+    
+else
+    rmpath(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'pai_results' ...
+        filesep 'time_results' filesep])
+end  
---
+++ NMF_codes/Matlab/TAES_codes/my_codes/time_eval_median.m
@@ -0,0 +1,116 @@
+clear;
+clc;
+close all;
+
+addpath(['..' filesep '..' filesep '.' filesep 'Sigtools' filesep])
+addpath(['..' filesep '..' filesep  '.' filesep 'Sigtools' filesep 'NMF_algorithms'])
+addpath(['..' filesep '..' filesep  'signalsGeneration' filesep]);
+addpath(['..' filesep '..' filesep  'signalsGeneration' filesep 'sim_params']);
+
+load sim_params_1.mat;
+
+params.fs = paramsSignal.Freqsamp;
+params.nfft = 16;
+params.nperseg = 16;
+params.overlap = params.nperseg - 1;
+params.hop_size = params.nperseg - params.overlap;
+params.window = ones(params.nperseg, 1);
+params.numberOfSources = 1;
+params.init = 'random';
+params.betaDivergence = 'kullback-leibler';
+params.numberOfIterations = 10000;
+params.tolChange = 1e-6;
+params.tolError = 1e-6;
+params.repetitions = 1;
+SNR = -25;
+params.JNRVector = -10;
+
+bandwidthVector = 2e6;
+periodVector = 8.62e-6;
+
+initialFrequency = 2e6;
+numberOfRawSamples = 4096;
+totalSamples = numberOfRawSamples;
+thresholdVector = 0.2;
+window_median_length_vector = 0;
+monteCarloLoops = 100;
+
+detection_res = zeros(monteCarloLoops, length(bandwidthVector), length(periodVector), ...
+    length(params.JNRVector), length(thresholdVector), length(window_median_length_vector));
+elapsedTime = zeros(monteCarloLoops, 1);
+
+GPSSignals = GPSGen(paramsSignal);
+GPSSignals = GPSSignals(1:numberOfRawSamples,:);
+GPSSignalsPower = pow_eval(GPSSignals);
+
+matNumber = 1;
+
+for loopIndex = 1:monteCarloLoops
+    mixtureSignal = zeros(totalSamples, length(params.JNRVector));
+    noise = randn(totalSamples, 1) + 1j*randn(totalSamples, 1);
+    noisePower = pow_eval(noise);
+    
+    for bandwidthIndex = 1:length(bandwidthVector)
+        for periodIndex = 1:length(periodVector)
+            paramsSignal.Noneperiod = round(periodVector(periodIndex)*params.fs);                   % number of samples with a sweep time
+            paramsSignal.IFmin = initialFrequency;                                                  % start frequency
+            paramsSignal.IFmax = bandwidthVector(bandwidthIndex) + initialFrequency;                    % end frequency
+            paramsSignal.foneperiod(1:paramsSignal.Noneperiod) = linspace(paramsSignal.IFmin, paramsSignal.IFmax, paramsSignal.Noneperiod);
+            paramsSignal.Initphase = 0;
+           
+            interferenceSignal = interferenceGen(paramsSignal);
+            interferenceSignal = interferenceSignal(1:numberOfRawSamples);
+            interferenceSignalPower = pow_eval(interferenceSignal);
+            
+            for i = 1:length(params.JNRVector)
+                GPSSignalsAux = GPSSignals;
+                interferenceSignalAux = interferenceSignal;
+                GPSMultiplier = sqrt(noisePower*10.^(SNR/10)./GPSSignalsPower);
+                mixtureGPS = sum(GPSSignalsAux.*GPSMultiplier, 2) + noise;
+                interferenceSignalAux = interferenceSignalAux*sqrt(noisePower*10^(params.JNRVector(i)/10)/interferenceSignalPower);
+                mixtureSignal(:,i) = mixtureGPS + interferenceSignalAux;
+            end
+            tstart = tic;
+            [W, ~, ~, PxxAux, ~, ~] = nmf_eval_v2(mixtureSignal, params);
+            
+            for JNRIndex = 1:length(params.JNRVector)
+                
+                inputNMF = abs(PxxAux{1, JNRIndex}).^2;
+                
+                inputNMF = inputNMF - mean(inputNMF);
+                inputNMF = inputNMF.*sqrt(1./var(inputNMF));
+                inputNMFAux = sqrt(sum(inputNMF.*inputNMF)) + eps;
+                inputNMFNormalised = inputNMF./inputNMFAux;
+                
+                WNormalised = W{1, JNRIndex}(:,1) - mean(W{1, JNRIndex}(:,1));
+                WNormalised = WNormalised.*sqrt(1./var(WNormalised));
+                WNormalised = WNormalised ./ (norm(WNormalised) + eps);
+                
+                output = inputNMFNormalised.'*WNormalised;
+                for thresholdIndex = 1:length(thresholdVector)
+                    for window_median_length_index = 1:length(window_median_length_vector)
+                        detection_res(loopIndex, bandwidthIndex, periodIndex, JNRIndex, thresholdIndex, window_median_length_index) = ...
+                            median(detection_eval(output, thresholdVector(thresholdIndex)));
+                        elapsedTime(loopIndex) = toc(tstart);
+                    end
+                end
+            end
+        end
+    end
+end
+
+if isunix
+    save(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+                'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+                filesep 'time_results' filesep 'resultsTime' num2str(matNumber) '.mat'], 'elapsedTime')
+else
+    save(['..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep '..' filesep 'Dropbox' filesep ...
+        'Doctorate' filesep 'Research' filesep 'data' filesep 'TAES_data' filesep 'new_data' filesep 'my_results' ...
+        filesep 'time_results' filesep 'resultsTime' num2str(matNumber) '.mat'], 'elapsedTime')
+end
+
+
+rmpath(['..' filesep '..' filesep '.' filesep 'Sigtools' filesep])
+rmpath(['..' filesep '..' filesep  '.' filesep 'Sigtools' filesep 'NMF_algorithms'])
+rmpath(['..' filesep '..' filesep  'signalsGeneration' filesep]);
+rmpath(['..' filesep '..' filesep  'signalsGeneration' filesep 'sim_params']);
