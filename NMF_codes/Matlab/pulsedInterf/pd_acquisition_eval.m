bf97e6b1832b086890486c53de641e4a5570a605
fbs2112 <fbs@pol.ufrj.br>
2020-02-05 19:21:20 +1100

    This needs to be finalised. Pfa aspects evaluation is missing

--- NMF_codes/Matlab/pulsedInterf/pd_acquisition_eval.m
+++ NMF_codes/Matlab/pulsedInterf/pd_acquisition_eval.m
@@ -1,24 +1,16 @@
-clear;
-clc;
-close all;
-
-set(groot, 'defaultAxesTickLabelInterpreter','latex');
-set(groot, 'defaultLegendInterpreter','latex');
-set(groot, 'defaulttextInterpreter','latex');
-
 addpath(['..' filesep 'signalsGeneration' filesep]);
 addpath(['..' filesep 'signalsGeneration' filesep 'sim_params']);
 addpath(['..' filesep 'Misc'])
 addpath(['..' filesep 'Sigtools' filesep])
 
-linewidth = 1.5;
-fontname = 'Times';
-fontsize = 24;
-dataPath = ['..' filesep 'figs' filesep '11-12' filesep];
-
+%%
+%Probability of detection evaluation
+clear;
+clc;
+close all;
+    
 load sim_params_2.mat;
-
-load(['.' filesep 'data' filesep 'nmf_testing_13.mat']);
+load(['.' filesep 'data' filesep 'nmf_testing_15.mat']);
 
 varN = 2;
 Pfa = 0.05;
@@ -34,22 +26,27 @@ paramsSignal.numberOfGPSSignals = 1;
 
 [~, locC] = GPSGen(paramsSignal);   % Resample the code at data sampling frequency
 
-JNRVector = [-15 -10 -5 10 30 50];
-JNRVector = [0];
+JNRVector = [-5 10 30 50];
+% JNRVector = [0];
 
 Tc = paramsSignal.Intetime;     % Coherent integration time in ms
 Nc = floor(Tc * fs);
 
 K = 1;  % Number of non-coherent integrations
 codeDl = (0:(Nc - 1)) / fs; % Code delays
+Carrphase = mod(2*pi*(trueDoppler)*codeDl,2*pi);
+Carrier = exp(1i*Carrphase).';
+monteCarloLoops = size(mixtureSignal, 3);
 
-monteCarloLoops = 100;
+thresPeakRatio = linspace(1e5, 9e5, 50);
+thresPeakRatioBorre = linspace(1, 10, 50);
+thresPAPR = linspace(5, 80, 50);
 
 % for nbitsIndex = 1:length(nbits)
 for loopIndex = 1:monteCarloLoops
     loopIndex
 %     locC = quantise_gps(locC, nbits(nbitsIndex));
-    for JNRIndex = 1:length(JNRVector)
+    for JNRIndex = 1:length(JNR)
         
         xHatBorio = mixtureSignal(:,JNRIndex,loopIndex);
         zeroedSamples(JNRIndex,loopIndex) = length(find(abs(xHatBorio) > thresholdBorio));
@@ -67,13 +64,157 @@ for loopIndex = 1:monteCarloLoops
                 Tsspace = DftParallelCodePhaseAcquisition( y, locC, Nc, Nd, DopStep, fs, fi );
                 sspace = sspace + Tsspace;  % Non-coherently accumulate the results
             end           
-            
+            signalPower = GPSSignals(i,:)*GPSSignals(i,:)'/length(GPSSignals(i,:));
             [maxVal(i,JNRIndex,loopIndex), DopInd] = max(max(sspace.'));
+            peakRatio(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex)/signalPower;
+            [peakVector, idx] = sort(sspace(DopInd,:), 'descend');
+            idxAux = idx(2:end);
+            minPeakDistance = round(fs*1e-6);
+            secondPeakIndexAux = find(abs(idxAux - idx(1)) > minPeakDistance, 1, 'first');
+            peakRatioBorre(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex)/sspace(DopInd,idxAux(secondPeakIndexAux));
+            
+            papr(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex) / mean(sspace(:));
+            
+            
+            GPSSignalNoDoppler = (GPSSignals(i,:).').*conj(Carrier);
+            locCDelayed = [locC(end - round(trueDelay*fs)+1:end) locC(1:end - round(trueDelay*fs))]; % Introducing artificial code delay
+            navSignalHat(i,JNRIndex,loopIndex) = GPSSignalNoDoppler.'*(locCDelayed).';
             
         end
     end
 end
+
 averagePeakValue = mean(maxVal, 3);
+averagePeakRatio = mean(peakRatio, 3);
+averagePeakRatioBorre = mean(peakRatioBorre, 3);
+averagePapr = mean(papr, 3);
+
+stdPeakValue = std(maxVal, 0, 3);
+stdPeakRatio = std(peakRatio, 0, 3);
+stdPeakRatioBorre = std(peakRatioBorre, 0, 3);
+stdPapr = std(papr, 0, 3);
+
+
+for i = 1:length(thresPeakRatio)
+    aux = peakRatio > thresPeakRatio(i);
+    tpPR(:,:,i) = mean(aux, 3);
+    fnPR(:,:,i) = mean(not(aux),3);
+    
+    aux = peakRatioBorre > thresPeakRatioBorre(i);
+    tpPRB(:,:,i) = mean(aux, 3);
+    fnPRB(:,:,i) = mean(not(aux),3);
+    
+    aux = papr > thresPAPR(i);
+    tpPAPR(:,:,i) = mean(aux, 3);
+    fnPAPR(:,:,i) = mean(not(aux),3);
+end
+
+for JNRIndex = 1:length(JNR)
+    for i = 1:3
+        
+        SNRHat(i,JNRIndex) = SNResti_pai(squeeze(navSignalHat(i,JNRIndex,:)));
+        SNRHat_SNV(i,JNRIndex) = SNResti_SNV(squeeze(navSignalHat(i,JNRIndex,:)));
+%         figure;
+%         histogram(peakRatio(i,JNRIndex,:), 10);
+%         figure;
+%         histogram(peakRatioBorre(i,JNRIndex,:), 10);
+%         figure;
+%         histogram(papr(i,JNRIndex,:), 10);
+    end
+end
+
+
+%%
+%Probability of false alarm evaluation
+clear;
+clc;
+close all;
+
+load sim_params_2.mat;
+load(['.' filesep 'data' filesep 'nmf_testing_12.mat']);
+
+varN = 2;
+Pfa = 0.05;
+thresholdBorio = sqrt(-2*varN*log(Pfa));
+fi = 0;
+fs = paramsSignal.Freqsamp;
+Nd = 81;
+DopStep = 125; %(Hz)
+
+trueDelay = 10e-6;
+trueDoppler = 1.5e3;
+paramsSignal.numberOfGPSSignals = 1;
+
+[~, locC] = GPSGen(paramsSignal);   % Resample the code at data sampling frequency
+
+JNR = [0];
+
+Tc = paramsSignal.Intetime;     % Coherent integration time in ms
+Nc = floor(Tc * fs);
+
+K = 1;  % Number of non-coherent integrations
+codeDl = (0:(Nc - 1)) / fs; % Code delays
+
+monteCarloLoops = size(mixtureSignal, 2);
+
+% for nbitsIndex = 1:length(nbits)
+for loopIndex = 1:monteCarloLoops
+    loopIndex
+%     locC = quantise_gps(locC, nbits(nbitsIndex));
+    for JNRIndex = 1:length(JNR)
+        
+        xHatBorio = mixtureSignal(:,loopIndex);
+        zeroedSamples(JNRIndex,loopIndex) = length(find(abs(xHatBorio) > thresholdBorio));
+        xHatBorio(abs(xHatBorio) > thresholdBorio) = 0;
+        
+        GPSSignals(1,:) = xHat(:,2,JNRIndex,loopIndex).';
+        GPSSignals(2,:) = xHatBorio;
+        GPSSignals(3,:) = mixtureSignal(:,loopIndex).';
+        
+        for i = 1:3
+            sspace = 0;                 % Search space were the results will be stored
+            for ii = 1:K
+                y = GPSSignals(i, (ii - 1) * Nc + (1:Nc) );   % use just 1 period of code at the time
+                % Compute the search space for a single coherent integration epoch
+                Tsspace = DftParallelCodePhaseAcquisition( y, locC, Nc, Nd, DopStep, fs, fi );
+                sspace = sspace + Tsspace;  % Non-coherently accumulate the results
+            end           
+            signalPower = GPSSignals(i,:)*GPSSignals(i,:)'/length(GPSSignals(i,:));
+            [maxVal(i,JNRIndex,loopIndex), DopInd] = max(max(sspace.'));
+            peakRatio(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex)/signalPower;
+            [peakVector, idx] = sort(sspace(DopInd,:), 'descend');
+            idxAux = idx(2:end);
+            minPeakDistance = round(fs*1e-6);
+            secondPeakIndexAux = find(abs(idxAux - idx(1)) > minPeakDistance, 1, 'first');
+            peakRatioBorre(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex)/sspace(DopInd,idxAux(secondPeakIndexAux));
+            
+            papr(i,JNRIndex,loopIndex) = maxVal(i,JNRIndex,loopIndex) / mean(sspace(:));
+        end
+    end
+end
+averagePeakValue = mean(maxVal, 3);
+averagePeakRatio = mean(peakRatio, 3);
+averagePeakRatioBorre = mean(peakRatioBorre, 3);
+averagePapr = mean(papr, 3);
+
+stdPeakValue = std(maxVal, 0, 3);
+stdPeakRatio = std(peakRatio, 0, 3);
+stdPeakRatioBorre = std(peakRatioBorre, 0, 3);
+stdPapr = std(papr, 0, 3);
+
+
+for JNRIndex = 1:length(JNR)
+    for i = 2:2
+        figure;
+        histogram(peakRatio(i,JNRIndex,:), 10);
+        figure;
+        histogram(peakRatioBorre(i,JNRIndex,:), 10);
+        figure;
+        histogram(papr(i,JNRIndex,:), 10);
+    end
+end
+
+%%
 
 rmpath(['..' filesep 'Sigtools' filesep])
 rmpath(['..' filesep 'Misc'])
