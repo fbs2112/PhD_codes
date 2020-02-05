8e47c336bc8d1fb7ae0d4effeb8dcb55d2e364fe
fbs2112 <fbs@pol.ufrj.br>
2020-02-05 19:20:31 +1100

    CNO evaluation has been fixed

--- NMF_codes/Matlab/pulsedInterf/test_GPS_CN0.m
+++ NMF_codes/Matlab/pulsedInterf/test_GPS_CN0.m
@@ -21,8 +21,7 @@ for i = 1:monteCarloLoops
 
     GPSMultiplier = sqrt(noisePower*10.^(SNR/10)./GPSSignalsPower);
     mixtureGPS = sum(GPSSignals.*GPSMultiplier, 2) + noise;
-
-    trueCN0(i) = pow2db((pow_eval((PRN.*PRN).')/noisePower)/Tc);
+    nav(i,1) = (PRN.*GPSMultiplier)*(PRN.*GPSMultiplier).';
 
     totalSamples = length(mixtureGPS);
     Timeofthisloop = 0:totalSamples-1;
@@ -31,26 +30,27 @@ for i = 1:monteCarloLoops
 
     mixtureGPSNoDoppler = (mixtureGPS.*conj(Carrier));
 
-    mixtureGPSNav = mixtureGPSNoDoppler.*PRN.';
-    signalPower = mixtureGPSNav'*mixtureGPSNav/length(mixtureGPSNav);
-    gpsSignalPowerHat = mean(abs(real(mixtureGPSNav)))^2;
-
-    CN0_SNV(i) = pow2db((gpsSignalPowerHat/(signalPower - gpsSignalPowerHat))/Tc);
-
-    Ta = Tc;
-    M = length(mixtureGPS);
-
-    Pn = sum(real(mixtureGPSNav))^2 + sum(imag(mixtureGPSNav))^2;
-    Pw = sum(real(mixtureGPSNav).^2 + imag(mixtureGPSNav).^2);
-
-    CN0_NWPR(i) = pow2db((M*(Pn/Pw) - 1) / (Ta * (M - (Pn/Pw))));
-    % M4 = mean(abs(mixtureGPSNav).^4);
-    % Pd = sqrt(2*(signalPower^2) - M4);
-    % CN0_MM = pow2db((Pd/(signalPower - Pd))/Tc);
+    mixtureGPSNav(i,1) = mixtureGPSNoDoppler.'*PRN.';
+   
 end
-CN0_SNV_av = mean(CN0_SNV);
-CN0_NWPR_av = mean(CN0_NWPR);
-trueCN0_av = mean(trueCN0); 
+trueSNR = pow2db(pow_eval(nav) / (noisePower*monteCarloLoops));
+
+signalPower = mixtureGPSNav'*mixtureGPSNav/length(mixtureGPSNav);
+gpsSignalPowerHat = mean(abs(real(mixtureGPSNav)))^2;
+
+SNR_SNV = pow2db((gpsSignalPowerHat/(signalPower - gpsSignalPowerHat)));
+
+Ta = Tc;
+M = length(mixtureGPS);
+
+Pn = sum(real(mixtureGPSNav))^2 + sum(imag(mixtureGPSNav))^2;
+Pw = sum(real(mixtureGPSNav).^2 + imag(mixtureGPSNav).^2);
+
+SNR_NWPR = pow2db((M*(Pn/Pw) - 1) / ((M - (Pn/Pw))));
+
+M4 = mean(abs(mixtureGPSNav).^4);
+Pd = sqrt(2*(signalPower^2) - M4);
+SNR_MM = pow2db((Pd/(signalPower - Pd)));
 
 rmpath(['..' filesep 'Sigtools' filesep])
 rmpath(['..' filesep 'signalsGeneration' filesep]);
