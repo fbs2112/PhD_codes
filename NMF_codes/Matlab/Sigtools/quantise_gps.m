7746c721231052d80793e28e6972a0568de0c559
fbs2112 <fbs@pol.ufrj.br>
2020-03-19 17:23:29 +1100

    Quantisation function has been fixed. Now it considers the agc gain, which is set according to Borios PhD thesis

--- NMF_codes/Matlab/Sigtools/quantise_gps.m
+++ NMF_codes/Matlab/Sigtools/quantise_gps.m
@@ -1,12 +1,38 @@
-function [quantSignal, mse] = quantise_gps(signal, nbits)
+function [quantSignal, mse] = quantise_gps(signal, nbits, varargin)
 
-minValue = min(signal);
-maxValue = max(signal);
+Ag = 1;
 
-imagPart = 0;
+if nargin == 3
+    noiseVar = varargin{1};
+    if isreal(signal)
+        noiseStd = sqrt(noiseVar);
+    else
+        noiseStd = sqrt(noiseVar / 2);
+    end
+    switch nbits
+        case 2
+            Ag = 1 / noiseStd;
+        case 3
+            Ag = 1.71 / noiseStd;
+        case 4
+            Ag = 2.98/noiseStd;
+        case 5
+            Ag = 5.315/noiseStd;
+        otherwise
+            Ag = 1;
+            fprintf('Warning, AGC gain not defined for nbits > 5\n');
+            fprintf('AGC gain set to 1');
+    end
+end
+
+signal = signal * Ag;
+maxValue = (2^nbits - 1);
+minValue = -maxValue;
+
 q = (maxValue-minValue)/(2^nbits-1);
 realPart = min(max(real(signal), minValue), maxValue);
 realPart = minValue + round((realPart-minValue)/q)*q;
+imagPart = 0;
 
 if ~isreal(signal)
     imagPart = min(max(imag(signal), minValue), maxValue);
