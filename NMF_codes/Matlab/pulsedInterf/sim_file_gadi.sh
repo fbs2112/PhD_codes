ec34e478db4798c069e54b75dae459787a605623
fbs2112 <fbs@poli.ufrj.br>
2020-05-13 14:42:34 +1000

    Mild updates on sim file

--- NMF_codes/Matlab/pulsedInterf/sim_file_gadi.sh
+++ NMF_codes/Matlab/pulsedInterf/sim_file_gadi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -q normal
-#PBS -l walltime=15:00:00
+#PBS -l walltime=3:00:00
 #PBS -l ncpus=16
 #PBS -l mem=32GB
-#PBS -l jobfs=5GB
+#PBS -l jobfs=1GB
 #PBS -l software=matlab_mq
 #PBS -l wd
  
 module load matlab/R2019b
-nohup matlab -nodisplay -nosplash -nojvm <test_chirp_mitigation_nmf_semi>& test_chirp_mitigation_nmf_semi&
+nohup matlab -nodisplay -nosplash -nojvm <test_chirp_mitigation_nmf_semi.m>& test_chirp_mitigation_nmf_semi&
