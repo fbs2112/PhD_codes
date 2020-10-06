fda8100ff69a78c79cb7c03ab3052b094ea535a5
fbs2112 <fbs@pol.ufrj.br>
2020-10-07 02:25:43 +1100

    KL divergence eval function now copes with L1 sparsity

--- NMF_codes/Matlab/Sigtools/NMF_algorithms/KL_divergence.m
+++ NMF_codes/Matlab/Sigtools/NMF_algorithms/KL_divergence.m
@@ -1,5 +1,16 @@
-function [res] = KL_divergence(X, W, H)
+%This function evaluates the Kullback-leibler divergence given the matrices
+%X, W and H.
+%
+%This code was created by Felipe Barboza da Silva
+% Copyright (c) School of Engineering. Macquarie University - Australia.
+% All rights reserved. 2020.
+% DISCLAIMER:
+%     This code is strictly private, confidential and personal to its recipients
+%     and should not be copied, distributed or reproduced in whole or in part,
+%     nor passed to any third party.
+% ** Do not duplicate or distribute without written permission from the owners. **
 
+function [res] = KL_divergence(X, W, H, varargin)
        
 WH = W*H;
 
@@ -7,3 +18,8 @@ div = X ./ WH;
 res = (X).*log(div);
 res = res + WH - X;
 res = sum(res(:));
+
+if nargin == 4
+    mu = varargin{1};
+    res = res + mu(1)*norm(W, 1) + mu(2)*norm(H, 1);
+end
