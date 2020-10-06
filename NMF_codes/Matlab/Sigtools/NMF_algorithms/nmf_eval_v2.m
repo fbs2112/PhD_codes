580958dac4a20d280243548d6522b20ccdcea97b
fbs2112 <fbs@pol.ufrj.br>
2020-10-07 02:26:57 +1100

    Several changes have been made. Now it supports several other parameters and different NMF algorithms. Check the documentation for further details

--- NMF_codes/Matlab/Sigtools/NMF_algorithms/nmf_eval_v2.m
+++ NMF_codes/Matlab/Sigtools/NMF_algorithms/nmf_eval_v2.m
@@ -1,7 +1,38 @@
+%This function performs the spectrogram evaluation and calls the NMF
+%decomposition function.
+%Inputs:
+%mixtureSignal: input signal. If more than one JNR value is used,
+%mixtureSignal must represent a matrix, where each column is composed by
+%the received signal for each JNR.
+%params: struct with the following mandatory fields:
+%        numberOfSources: defines the decomposition rank
+%        init: 'random' or 'custom'
+%        betaDivergence: either 'kullback-leibler' or 'euclidean'
+%        numberOfIterations: max number of iterations
+%        tolChange: tolerance parameter that defines convergence
+%        tolError: error parameter that defines convergence
+% 
+%Outputs:
+%W2: cell containing the corresponding W matrix for each JNR value
+%H2: cell containing the corresponding H matrix for each JNR value
+%ReconstructError2: cell containing the corresponding reconstruction error 
+%for each JNR value
+%dataCell: cell containing the magnitude spectrogram for each JNR value
+%f: frequency vector for spectrogram plotting
+%t: time vector for spectrogram plotting
+%data: cell containing the input signal for each JNR value
+%
+%This code was created by Felipe Barboza da Silva
+% Copyright (c) School of Engineering. Macquarie University - Australia.
+% All rights reserved. 2020.
+% DISCLAIMER:
+%     This code is strictly private, confidential and personal to its recipients
+%     and should not be copied, distributed or reproduced in whole or in part,
+%     nor passed to any third party.
+% ** Do not duplicate or distribute without written permission from the owners. **
+
+
 function [W2, H2, reconstructError2, dataCell, f, t, data] = nmf_eval_v2(mixtureSignal, params, varargin)
-%New NMF evaluation function
-%Differences from nmf_eval:
-    %Input signal may be corrupted by noise
 
 dataCellLength = 1;
 
@@ -25,6 +56,18 @@ if ~isfield(params, 'reassigned')
     params.reassigned = false;
 end
 
+if ~isfield(params, 'centered')
+    params.centered = true;
+end
+
+if ~isfield(params, 'mu')
+    params.mu = [0 0];
+end
+
+if ~isfield(params, 'transpose')
+    params.transpose = false;
+end
+
 if nargin > 2 && varargin{1}
     dataCellLength = 2;
 end
@@ -40,11 +83,24 @@ for i = 1:size(mixtureSignal, 2)
     data{1, i} = mixtureSignal(:,i);
     
     if params.reassigned
-        [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, 'centered', params.specType, 'reassigned');
+        if params.centered
+            [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, 'centered', params.specType, 'reassigned');
+        else
+            [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, params.specType, 'reassigned');
+        end
     else
-        [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, 'centered', params.specType);
+        if params.centered
+            [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, 'centered', params.specType);
+        else 
+            [PxxAux, f, t] = spectrogram(data{1, i}, params.window, params.overlap, params.nfft, params.fs, params.specType);
+        end
     end
+    
     dataCell{1, i} = PxxAux;
+    
+    if params.transpose
+        dataCell{1, i} = dataCell{1, i}.';
+    end
 
     if nargin == 3 
         if varargin{1}
@@ -84,8 +140,14 @@ for i = 1:size(mixtureSignal, 2)
             switch params.alg
                 case 'vanilla'
                     [W, H, reconstructError] = nmf_vanilla(inputNMF, params);
+                case 'vanilla_sparse_H'
+                    [W, H, reconstructError] = nmf_vanilla_sparse_H(inputNMF, params);
                 case 'snmf'
                     [W, H, reconstructError] = nmf_sparse_well_done(inputNMF, params);
+                case 'vanilla_semi'
+                    [W, H, reconstructError] = nmf_vanilla_semi(inputNMF, params);
+                case 'snmf_semi'
+                    [W, H, reconstructError] = nmf_sparse_well_done_semi(inputNMF, params);
                 otherwise
                     error('NMF algorithm not supported');
             end
