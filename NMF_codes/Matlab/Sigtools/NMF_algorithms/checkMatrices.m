2d83604675121ac7003bbc1b21b86b82bd148402
fbs2112 <fbs@pol.ufrj.br>
2020-10-07 02:25:01 +1100

    Documentation has been updated

--- NMF_codes/Matlab/Sigtools/NMF_algorithms/checkMatrices.m
+++ NMF_codes/Matlab/Sigtools/NMF_algorithms/checkMatrices.m
@@ -1,3 +1,15 @@
+%This function checks whether the input matrices W and H have negative
+%elements. If that is the case, it returns true, and false otherwise
+%
+%This code was created by Felipe Barboza da Silva
+% Copyright (c) School of Engineering. Macquarie University - Australia.
+% All rights reserved. 2020.
+% DISCLAIMER:
+%     This code is strictly private, confidential and personal to its recipients
+%     and should not be copied, distributed or reproduced in whole or in part,
+%     nor passed to any third party.
+% ** Do not duplicate or distribute without written permission from the owners. **
+
 function [flag] = checkMatrices(W, H)
 
 W = W(:);
